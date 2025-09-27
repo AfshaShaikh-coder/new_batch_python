a0ed68ec60103d5fb742991c5ae2fad9cbddb595
AfshaShaikh-coder <izzakhan8698@gmail.com>
2025-09-27 15:17:39 +0530

    Initial commit

---
+++ list.py
@@ -0,0 +1,76 @@
+# int,float,string,bool
+
+l=["hello",1,True]
+print(l)
+print(type(l))
+
+#print(l.append('sana'))#None
+l.append("sana")
+
+print(l)#['hello', 1, True, 'sana']
+
+print(l)#['hello', 1, True, 'sana']
+
+print(l[0])# hello
+print(l[-1])# sana
+
+l[0]="hello python"
+print(l)
+
+l.insert(2,100)# 2 arg---->1 arg position ---2nd args--elemnts
+print(l)
+
+l.append(400)# ---> end elem +add
+print(l)
+l.extend([1,2,3,4,5,6,7,8,9,10])
+print(l)
+l.append([100,200,300,400,500])
+print(l)
+l.remove("sana")
+print(l)
+l.pop()
+print(l)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""str_var="hello"
+print(str_var.upper())
+print(str_var)
+
+var_new=str_var.upper()
+print(var_new)
+print(var_new)
+
+"""
+
+
+
+
+
+
